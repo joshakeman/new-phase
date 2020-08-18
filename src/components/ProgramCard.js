186897daa9ef25c29645732be7612f8fe867ac33
Josh Akeman <joshakeman@gmail.com>
2020-08-17 20:29:51 -0400

    finished draft of first program card

--- src/components/ProgramCard.js
+++ src/components/ProgramCard.js
@@ -4,9 +4,12 @@ export default function ProgramCard({ direction, imgSrc, title, copy}) {
     return (
         <div class="program-card">
             <h2>{title}</h2>
-            <div class="content-wrapper">
+            <div class="content-wrap">
                 <img src={imgSrc} />
-                <p>{copy}</p>
+                <div class="btn-para-wrap">
+                    <p>{copy}</p>
+                    <div class="btn">Learn More</div>
+                </div>
             </div>
         </div>
     )
--- src/components/bullet.js
+++ src/components/bullet.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Bullet() {
+export default function Bullet({ bulletText }) {
     return (
-        <div class="bullet">
-            
+        <div class="bullet-wrap">
+            <div class="bullet"/>
+            <h4 class="bullet-text">
+                {bulletText}
+            </h4>
         </div>
     )
 }
