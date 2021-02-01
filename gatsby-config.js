858c5786790c2ddedd16781dc3702bd9a29de830
Josh Akeman <joshakeman@gmail.com>
2021-01-31 21:53:06 -0500

    got sanity hookup working... rendering blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/npf_logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "azfe7mc7",
+        dataset: "production",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
