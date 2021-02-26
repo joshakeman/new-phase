ef0ee65483c951b381c6bec0b866d2cfc9cef143
Josh Akeman <joshakeman@gmail.com>
2021-02-25 19:31:50 -0500

    modified site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `New Phase Fitness`,
+    description: `New Phase Fitness offers customized in-home or virtual personal training sessions and nutrition coaching for women.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -19,20 +19,19 @@ export default function AboutMe({ data }) {
                    <h1 className="Festivo">About Me</h1>
                    <img class="mobile-about-me-img" src={annie} />
                    <h4 style={{fontWeight: 100}}>
-                   I am a certified personal trainer, women’s health specialist, pre-post natal fitness specialist, and nutrition coach.
+                   I am a certified personal trainer, women’s fitness specialist, pre-post natal fitness specialist, and nutrition coach.
                    </h4>
                    <br></br>
                    <h4 style={{fontWeight: 100}}>
-                   My fitness journey began with a love of dance when I was two years old. I went on to major in Dance in college, and taught many classes for varying age groups. 
-                   </h4>
+                   My fitness journey began with a love of dance when I was two years old. I went on to major in Dance during college, and taught many classes for varying age groups.
+
+                  </h4>
                    <br></br>
                    <h4 style={{fontWeight: 100}}>
-                   After giving birth to my son, I began to realize the lack of adequate physical care available to women during these times of physical transition, such as before and after giving birth and the time leading up to and following menopause. Women’s bodies undergo such powerful physical transformations, and I felt this fact was often unrecognized within the fitness industry.  
-                   </h4>
+                   After giving birth to my son, I experienced the lack of adequate physical fitness care available to women during times of transition, such as before and after giving birth and the time leading up to and following menopause. Women’s bodies undergo such powerful physical transformations, and I felt this fact often went unrecognized within the fitness industry.                   </h4>
                    <br></br>
                    <h4 style={{fontWeight: 100}}>
-                   Because of this, I felt women were not being given the kind of tailored exercise programming they truly deserved. This thought led me to create New Phase Fitness in the hopes of providing the kind of support that everyone deserves during these pivotal phases.
-                   </h4>
+                   Women are not always given the kind of tailored exercise programming they truly need. This thought led me to create New Phase Fitness in the hopes of providing the kind of support that everyone deserves during these pivotal phases.                   </h4>
                    <br></br>
                    <div className="qualifications">
                     <h6>B.A. in Dance from Franklin & Marshall College</h6>
